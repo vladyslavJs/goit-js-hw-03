3264ef70a0661fa50a99a6fbf8f768ac9c6b96f8
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-10-30 11:26:59 +0100

    create-task-3

--- js/task-3.js
+++ js/task-3.js
@@ -19,7 +19,7 @@ function filterArray(numbers, value) {
     return newArrayNumbers;    
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 3));
 console.log(filterArray([1, 2, 3, 4, 5], 4)); 
 console.log(filterArray([1, 2, 3, 4, 5], 5)); 
 console.log(filterArray([12, 24, 8, 41, 76], 38)); 
