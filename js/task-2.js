55f28a98c283b0f3eae4f155a64c3d77de854a0b
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-10-30 11:26:38 +0100

    create-task-2

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,7 @@ function makeArray(firstArray, secondArray, maxLength) {
     }    
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
