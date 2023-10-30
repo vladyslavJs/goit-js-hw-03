355d535c59c436c48cf0cb10ec0877eab0708270
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-10-30 11:25:37 +0100

    create-task-1

--- js/task-1.js
+++ js/task-1.js
@@ -13,7 +13,7 @@ function slugify(title) {
     return slug.join('-');
 }
 
-console.log(slugify("Arrays for begginers")); 
+console.log(slugify("Arrays for begginers"));
 console.log(slugify("English for developer")); 
 console.log(slugify("Ten secrets of JavaScript")); 
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
